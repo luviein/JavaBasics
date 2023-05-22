aa6a6b77d91e1d2695e55553680b07958caf33b9
Yenleng <yenlengggggg@gmail.com>
2023-05-22 21:24:32 +0800

    add array example

---
+++ Arrays/ArrayExample.java
@@ -0,0 +1,18 @@
+package Arrays;
+
+public class ArrayExample {
+
+    //diff ways to create array
+    
+    // cam only store same data type
+    /* 
+    int bucky[] = new int[10]; //indicates how many numbers you wanna put in the int array
+    bucky[0] = 100;
+    bucky[1] = 40;
+    bucky[9] = 3;
+    */
+    public static void main(String[] args) {
+        int bucky[] = {1,2,3,4,5};    
+    }
+    
+}
