cf39fb033fdb728a5471b5a0b05a73e247309e6e
Yenleng <yenlengggggg@gmail.com>
2023-05-22 21:45:27 +0800

    add summing array elements example

--- Arrays/ArrayExample.java
+++ Arrays/ArrayExample.java
@@ -2,17 +2,26 @@ package Arrays;
 
 public class ArrayExample {
 
-    //diff ways to create array
-    
+    // diff ways to create array
+
     // cam only store same data type
-    /* 
-    int bucky[] = new int[10]; //indicates how many numbers you wanna put in the int array
-    bucky[0] = 100;
-    bucky[1] = 40;
-    bucky[9] = 3;
-    */
+    /*
+     * int bucky[] = new int[10]; //indicates how many numbers you wanna put in the
+     * int array
+     * bucky[0] = 100;
+     * bucky[1] = 40;
+     * bucky[9] = 3;
+     */
     public static void main(String[] args) {
-        int bucky[] = {1,2,3,4,5};    
+        int bucky[] = { 1, 2, 3, 4, 5 };
+
+        // summing elements of array (5 + 4 + 3 + 2 + 1)
+        int sum = 0;
+        for (int counter = 0; counter < bucky.length; counter++) {
+            sum += bucky[counter];
+
+        }
+        System.out.println("Sum of these numbers: " + sum);
     }
-    
+
 }
